abc55167475329f22d61f60cb2fd083f286ecc5b
Lana Virina <lana.virina@gmail.com>
2023-03-18 20:25:24 +0300

    Add tests for Stack & Quevue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+from src.queue import Queue
+import unittest
+from types import NoneType
+
+
+class TestQueue(unittest.TestCase):
+
+    def test_queue_str(self):
+        qu = Queue()
+        qu.enqueue('first')
+        qu.enqueue('second')
+        qu.enqueue('third')
+        self.assertEqual(str(qu), "first\nsecond\nthird")
+
+    def test_queue_enqueue(self):
+        qu = Queue()
+        qu.enqueue('first')
+        qu.enqueue('second')
+        qu.enqueue('third')
+        self.assertIs(qu.tail.data, "third")
+        self.assertIsInstance(qu.tail.next_node, NoneType)
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,28 +1,36 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
-from src import stack
+from src.stack import Stack, Node
 import unittest
 
+
 class TestStack(unittest.TestCase):
+
+    def test_stack_str(self):
+        st = Stack()
+        st.push('first')
+        st.push('second')
+        st.push('third')
+        self.assertEqual(str(st), "Stack (third)")
+
     def test_node(self):
-        n1 = stack.Node(10, None)
-        n2 = stack.Node('b', n1)
+        n1 = Node(10, None)
+        n2 = Node('b', n1)
         self.assertIsNone(n1.next_node)
         self.assertIs(n1, n2.next_node)
 
-    def test_stack(self):
-        st = stack.Stack()
+    def test_stack_push(self):
+        st = Stack()
         st.push('first')
         st.push('second')
         st.push('third')
         self.assertIs(st.top.data, "third")
 
     def test_stack_pop(self):
-        st = stack.Stack()
+        st = Stack()
         st.push('first')
         node_1 = st.top
         st.push('second')
         node_2 = st.top
         self.assertIs(st.pop(), node_2.data)
         self.assertIs(st.pop(), node_1.data)
-
 
